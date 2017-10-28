56b729af41cc01ff458278b6604ec6f9b8d1fc23
Lior Moshe <liorm03@gmail.com>
2017-10-28 18:51:06 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/apps/mosbros/dogs_app_temp_name/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.apps.mosbros.dogs_app_temp_name;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
